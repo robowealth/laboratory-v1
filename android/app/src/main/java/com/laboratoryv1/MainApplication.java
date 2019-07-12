f55a72e6d1a4df7f1886c3acfb3b418da32a8b3a
Suphawut Pitirit <suphawut@robowealth.co.th>
2019-07-12 18:28:22 +0700

    upgrade

--- android/app/src/main/java/com/laboratoryv1/MainApplication.java
+++ android/app/src/main/java/com/laboratoryv1/MainApplication.java
@@ -3,6 +3,7 @@ package com.laboratoryv1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnativenavigation.bridge.NavigationReactPackage;
@@ -17,7 +18,6 @@ import fr.snapp.imagebase64.RNImgToBase64Package;
 import com.rnfs.RNFSPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import org.reactnative.camera.RNCameraPackage;
 import com.parryworld.rnappupdate.RNAppUpdatePackage;
 import com.appsflyer.reactnative.RNAppsFlyerPackage;
 import com.appsee.reactnative.AppseeReactPackage;
@@ -25,16 +25,22 @@ import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPa
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.PTR.IDFA.IDFAPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationActivity;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends Application implements NavigationActivity {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -46,6 +52,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new RSSignatureCapturePackage(),
@@ -59,15 +66,13 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFSPackage(),
             new RNFetchBlobPackage(),
             new RNDeviceInfo(),
-            new RNCameraPackage(),
             new RNAppUpdatePackage(),
             new RNAppsFlyerPackage(),
             new AppseeReactPackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this),
-            new IDFAPackage(),
-            new RNGestureHandlerPackage()
+            new IDFAPackage()
       );
     }
 
@@ -87,4 +92,43 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
+
+      @Override
+    protected ReactGateway createReactGateway() {
+            ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+            @Override
+            protected String getJSMainModuleName() {
+                        return "index";
+                    }
+            };
+            return new ReactGateway(this, isDebug(), host);
+    }
+
+            @Override
+    public boolean isDebug() {
+            return BuildConfig.DEBUG;
+        }
+
+            protected List<ReactPackage> getPackages() {
+            // Add additional packages you require here
+                    // No need to add RnnPackage and MainReactPackage
+                            return Arrays.<ReactPackage>asList(
+                        // eg. new VectorIconsPackage()
+                            );
+        }
+
+            @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+            return getPackages();
+        }
+
+          @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+            return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+                       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+                      }
+    };
+          }
 }
