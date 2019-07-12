cfa26e8c2594cab1f4810705760dcb1332886321
Suphawut Pitirit <suphawut@robowealth.co.th>
2019-07-12 12:03:42 +0700

    Initial

--- android/app/src/main/java/com/laboratoryv1/MainApplication.java
+++ android/app/src/main/java/com/laboratoryv1/MainApplication.java
@@ -3,6 +3,28 @@ package com.laboratoryv1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.horcrux.svg.SvgPackage;
+import com.reactnativenavigation.bridge.NavigationReactPackage;
+import com.rssignaturecapture.RSSignatureCapturePackage;
+import io.sentry.RNSentryPackage;
+import org.wonday.pdf.RCTPdfView;
+import com.reactnativenavigation.NavigationReactPackage;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.jimmydaddy.imagemarker.ImageMarkerPackage;
+import fr.snapp.imagebase64.RNImgToBase64Package;
+import com.rnfs.RNFSPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import org.reactnative.camera.RNCameraPackage;
+import com.parryworld.rnappupdate.RNAppUpdatePackage;
+import com.appsflyer.reactnative.RNAppsFlyerPackage;
+import com.appsee.reactnative.AppseeReactPackage;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+import com.PTR.IDFA.IDFAPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +46,27 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new SvgPackage(),
+            new RSSignatureCapturePackage(),
+            new RNSentryPackage(MainApplication.this),
+            new RCTPdfView(),
+            new NavigationReactPackage(),
+            new ReactNativeLocalizationPackage(),
+            new ImageResizerPackage(),
+            new ImageMarkerPackage(),
+            new RNImgToBase64Package(),
+            new RNFSPackage(),
+            new RNFetchBlobPackage(),
+            new RNDeviceInfo(),
+            new RNCameraPackage(),
+            new RNAppUpdatePackage(),
+            new RNAppsFlyerPackage(),
+            new AppseeReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
+            new IDFAPackage(),
             new RNGestureHandlerPackage()
       );
     }
